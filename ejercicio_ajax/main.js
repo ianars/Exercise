49178e003ed90e59eeba8b9f1ea50c513b5c6f0f
ianars <iana.rusu.it@gmail.com>
2017-06-07 20:19:41 +0200

    ejercicio ajax actualizado

--- ejercicio_ajax/main.js
+++ ejercicio_ajax/main.js
@@ -9,21 +9,21 @@ var request = new XMLHttpRequest();
 
 btn.addEventListener("click", requestInfo);
 
-request.onload = requestInfo;
 function requestInfo() {
-  if (request.status >= 200 && request.status < 400) {
-    var data = JSON.parse(request.responseText);
-    request.open('GET', 'https://api.github.com/users/' +userId.value, true);
-    userName.innerHTML = data.login;
-    picture.src = data.avatar_url;
-    num.innerHTML = data.public_repos;
-  } else {
-    console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
-  }
-}
-
-request.onerror = function() {
-  console.log('Error al tratar de conectarse con el servidor');
-};
+  request.open('GET', 'https://api.github.com/users/' +userId.value, true);
+  request.onload = function(){
+    if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(request.responseText);
+      userName.innerHTML = data.login;
+      picture.src = data.avatar_url;
+      num.innerHTML = data.public_repos;
+    } else {
+      console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
+    }
+  };
+  request.onerror = function() {
+    console.log('Error al tratar de conectarse con el servidor');
+  };
 //3-request.send: manda la petición al servidor
-request.send();
+  request.send();
+}
